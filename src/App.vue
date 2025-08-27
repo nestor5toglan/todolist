e19bc547cb83ba05a9a973e198c1ccbc0a54b748
NESTARS <66269490+nestor5toglan@users.noreply.github.com>
2025-08-27 19:54:05 +0900

    Update App.vue
    test video1

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
   "
 >
   <iframe width="560" height="315" 
-    src="https://www.youtube.com/embed/ID_DE_TA_VIDEO" 
+    src="https://www.youtube.com/watch?v=Il8MfntqUXY" 
     title="Video player" frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
     allowfullscreen>
